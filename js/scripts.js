9936c9e25b531fbd629f60377fcfc18dbeec5163
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-02-22 18:25:14 -0800

    First foundational elements of a vacation quiz using HTML, CSS, and JQuery

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("form#politicmeter").submit(function(event) {
+
+    var answer = "";
+      if (abortion === 'prolife' && gay === 'antigay' && gun === 'progun');
+      answer = "Republican";
+
+    var answer = "";
+      if (abortion === 'prochoice' && gay === 'progay' && gun === 'antigun');
+      answer = "Democrat";
+
+      $("#meter").empty().append(quote);
+      $("#answer").show();
+
+    event.preventDefault();
+  });
+});
