7e3f7be1baf9af80edd52c29b845b2f0cd226570
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-02-25 14:23:49 -0600

    Changed to radio buttons, added radio definitions to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
   $("form#vacationquiz").submit(function(event) {
 
+    var terrain = $("input:radio[name=terrain]:checked").val();
+    var transporting = $("input:radio[name=transporting]:checked").val();
+    var development = $("input:radio[name=development]:checked").val();
+    var language = $("input:radio[name=language]:checked").val();
+    var food = $("input:radio[name=food]:checked").val();
+
     var answer = "";
       if (terrain === 'mountains' && transporting === 'hiking' && development === 'remote' && language === 'comfortable' && food === 'notpriority') {
-      answer = "Kathmandu, Nepal";
+      answer = "A. Kathmandu, Nepal";
     } else if (terrain === 'city' && transporting === 'car' && development === 'somewhat' && language === 'little' && food === 'bigpriority') {
-      answer = "Florence, Italy";
+      answer = "B. Florence, Italy";
     } else if (terrain === 'beaches' && transporting === 'taxi' && development === 'developed' && language === 'uncomfortable' && food === 'western') {
-      answer = "Sydney, Australia";
+      answer = "C. Sydney, Australia";
     }
       $("#answer").show();
       $("#meter").text(answer);
