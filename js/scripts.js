40dec4f1ad2f9c798819cde1b67705c994394a48
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-02-25 13:17:33 -0600

    Additional HTML variance

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,13 @@ $(document).ready(function() {
   $("form#vacationquiz").submit(function(event) {
 
     var answer = "";
-      if (terrain === 'mountains' && transporting === 'hiking' && development === 'remote' && language === 'comfortable' && food === 'notpriority');
-      answer = "Kathmandu";
-
-    var answer = "";
-      else if (terrain === 'city' && transporting === 'car' && development === 'somewhat' && language === 'little' && food === 'bigpriority');
-      answer = "Florence";
-
-    var answer = "";
-      else (terrain === 'beaches' && transporting === 'taxi' && development === 'developed' && language === 'uncomfortable' && food === 'western');
-      answer = "Sydney";
-
+      if (terrain === 'mountains' && transporting === 'hiking' && development === 'remote' && language === 'comfortable' && food === 'notpriority') {
+      answer = "Kathmandu, Nepal";
+    } else if (terrain === 'city' && transporting === 'car' && development === 'somewhat' && language === 'little' && food === 'bigpriority') {
+      answer = "Florence, Italy";
+    } else if (terrain === 'beaches' && transporting === 'taxi' && development === 'developed' && language === 'uncomfortable' && food === 'western') {
+      answer = "Sydney, Australia";
+    }
       $("#answer").show();
       $("#meter").text(answer);
 
