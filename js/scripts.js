f022783efb8931c3ea4196e8d2baaaf57a828a4a
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-02-28 19:45:09 -0600

    Attempting to work out issue with Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -12,22 +12,44 @@ $(document).ready(function() {
       $("#answer2").hide()
       $("#answer3").hide();
       $("#answer4").hide();
+      $("#answer5").hide();
+      $("#answer6").hide();
       $("#answer1").fadeIn();
     } else if (terrain === 'city' && transporting === 'car' && development === 'somewhat' && language === 'little' && food === 'bigpriority') {
       $("#answer1").hide()
       $("#answer3").hide();
       $("#answer4").hide();
+      $("#answer5").hide();
+      $("#answer6").hide();
       $("#answer2").fadeIn();
     } else if (terrain === 'beaches' && transporting === 'taxi' && development === 'developed' && language === 'uncomfortable' && food === 'western') {
       $("#answer1").hide()
       $("#answer2").hide();
       $("#answer4").hide();
+      $("#answer5").hide();
+      $("#answer6").hide();
       $("#answer3").fadeIn();
+    } else if (terrain === 'mountains' && transporting === 'hiking' && development === 'developed' && language === 'comfortable' && food === 'western') {
+      $("#answer1").hide()
+      $("#answer2").hide();
+      $("#answer3").hide();
+      $("#answer5").hide();
+      $("#answer6").hide();
+      $("#answer4").fadeIn();
+    } else if (terrain === 'city' && transporting === 'taxi' && development === 'developed' && language === 'comfortable' && food === 'bigpriority') {
+      $("#answer1").hide()
+      $("#answer2").hide();
+      $("#answer3").hide();
+      $("#answer4").hide();
+      $("#answer6").hide();
+      $("#answer5").fadeIn();
     } else {
       $("#answer1").hide()
       $("#answer2").hide();
       $("#answer3").hide();
-      $("#answer4").fadeOut();
+      $("#answer4").hide();
+      $("#answer5").hide();
+      $("#answer6").fadeOut();
     }
 
     $("#answer").show();
